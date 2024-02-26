db89d82f9883a01f76b8de41ca39bb5c07f379d3
Charly-programmeur <charlynoudjalta.com>
2024-02-26 16:15:27 +0000

    checkpoint

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Paris Saint-Germain",
+    nationality: "Argentinian",
+    jerseyNumber: 30,
+    age: 34,
+    imageUrl: "https://s.france24.com/media/display/a6443f2a-eed0-11ea-a488-005056a98db9/w:1280/p:1x1/1916ffb4fe5874d2d332ae652632f986453c6f0c.jpg",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Manchester United",
+    nationality: "Portuguese",
+    jerseyNumber: 7,
+    age: 37,
+    imageUrl: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt31f4cb1893bca44b/652a25761644e9ffe4fd6247/1734016483_2.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+  },
+  {
+    name: "Neymar Jr.",
+    team: "Paris Saint-Germain",
+    nationality: "Brazilian",
+    jerseyNumber: 10,
+    age: 30,
+    imageUrl: "https://assets-fr.imgfoot.com/media/cache/1200x1200/psg-om-ney-2223-img1.jpg",
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "French",
+    jerseyNumber: 7,
+    age: 23,
+    imageUrl: "https://static.lematin.ma/files/lematin/images/articles/2022/10/f599f88d925497ae90c488fd1c5cf8ad.jpg",
+  },
+];
+
+export default players;
